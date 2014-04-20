d0938e213141e0276b1a94f90db9d565ae25bed2
rochefort <terasawan@gmail.com>
2014-04-21 01:09:39 +0900

    Initial Commit

---
+++ WeightControl/WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WeightControl
+//
+//  Created by trsw on 2014/04/21.
+//  Copyright (c) 2014年 trsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeightControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeightControl
+//
+//  Created by trsw on 2014/04/21.
+//  Copyright (c) 2014年 trsw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WCAppDelegate class]));
+    }
+}
