b7dcb64f1d101eb9b79616efab821ce37d889b20
rochefort <terasawan@gmail.com>
2014-04-24 00:49:33 +0900

    Add FMDB for using sqlite by cocoapods

---
+++ WeightControl/WCDataManager.h
@@ -0,0 +1,13 @@
+//
+//  WCDataManager.h
+//  WeightControl
+//
+//  Created by trsw on 2014/04/23.
+//  Copyright (c) 2014年 trsw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCDataManager : NSObject
+
+@end
---
+++ WeightControl/WCDataManager.m
@@ -0,0 +1,13 @@
+//
+//  WCDataManager.m
+//  WeightControl
+//
+//  Created by trsw on 2014/04/23.
+//  Copyright (c) 2014年 trsw. All rights reserved.
+//
+
+#import "WCDataManager.h"
+
+@implementation WCDataManager
+
+@end
